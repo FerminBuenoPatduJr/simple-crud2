8b4a54559290d5dbaec1185ce122703291040a89
ferminpatdujr <fpatdu@77global.biz>
2022-08-13 22:38:40 +0800

    yes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,26 @@ import { BlogItemComponent } from './modules/blog/components/blog-item/blog-item
 import { BlogListComponent } from './modules/blog/pages/blog-list/blog-list.component';
 import { UserModule } from './modules/user/user.module';
 
-
 const routes: Routes = [
-{
-  path: "blog",
-  loadChildren: () => import("./modules/blog/blog.module").then(m => m.BlogModule) 
-},
-{
-  path: "book",
-  loadChildren: () => import("./modules/book/book.module").then(m => m.BookModule)
-},
-{
-  path: "user",
-  loadChildren: () => import("./modules/user/user.module").then(m => m.UserModule)
-}
-  
+  {
+    path: 'blog',
+    loadChildren: () =>
+      import('./modules/blog/blog.module').then((m) => m.BlogModule),
+  },
+  {
+    path: 'book',
+    loadChildren: () =>
+      import('./modules/book/book.module').then((m) => m.BookModule),
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./modules/user/user.module').then((m) => m.UserModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
