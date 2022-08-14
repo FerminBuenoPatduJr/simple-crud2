84f48d0bc601d49faf4843796208eda66813a8f5
ferminpatdujr <fpatdu@77global.biz>
2022-08-14 17:04:13 +0800

    yes

--- src/app/modules/blog/components/blog-item/blog-item.component.ts
+++ src/app/modules/blog/components/blog-item/blog-item.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Blog } from '../../models/blog.interface';
 import { BlogService } from '../../services/blog.service';
@@ -9,14 +8,29 @@ import { BlogService } from '../../services/blog.service';
   styleUrls: ['./blog-item.component.scss'],
 })
 export class BlogItemComponent implements OnInit {
-  @Input() blogItem: Blog | undefined;
+  @Input() blog:
+    | {
+        id: number;
+        title: string;
+        description: string;
+        author: string;
+        comments: string[];
+      }
+    | undefined;
   @Output() actionEmitter = new EventEmitter<Blog>();
-  blogList: Blog[] | undefined;
+  blogList: Blog[] = [];
 
   constructor(private blogService: BlogService) {}
 
   ngOnInit(): void {}
 
+  button1() {
+    this.actionEmitter.emit(this.blog);
+  }
+
+  danger() {
+    this.actionEmitter.emit(this.blog);
+  }
   getBlogs() {
     this.blogList = this.blogService.getBlogs();
   }
--- src/app/modules/blog/pages/blog-list/blog-list.component.ts
+++ src/app/modules/blog/pages/blog-list/blog-list.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Blog } from '../../models/blog.interface';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
-  styleUrls: ['./blog-list.component.scss']
+  styleUrls: ['./blog-list.component.scss'],
 })
 export class BlogListComponent implements OnInit {
+  blogs: {
+    id: number;
+    title: string;
+    description: string;
+    author: string;
+    comments: string[];
+  }[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private blogService: BlogService) {
+    this.blogs = this.blogService.getBlogs();
   }
+  ngOnInit(): void {}
 
+  executeAction(blog: Blog) {
+    console.log(blog.id);
+  }
 }
