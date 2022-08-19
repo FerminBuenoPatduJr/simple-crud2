f34aabe42802397eab0a870f0564d462ff342cfd
ferminpatdujr <fpatdu@77global.biz>
2022-08-19 22:20:37 +0800

    push

--- src/app/modules/blog/pages/blog-list/blog-list.component.ts
+++ src/app/modules/blog/pages/blog-list/blog-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Blog } from '../../models/blog.interface';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { BlogService } from '../../services/blog.service';
-import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-blog-list',
@@ -10,18 +9,26 @@ import {MatCardModule} from '@angular/material/card';
   styleUrls: ['./blog-list.component.scss'],
 })
 export class BlogListComponent implements OnInit {
-  blogs: {
-    id: number;
-    title: string;
-    description: string;
-    author: string;
-    comments: string[];
-  }[] = [];
+  blogs: // {
+  //   id: number;
+  //   title: string;
+  //   description: string;
+  //   author: string;
+  //   comments: string[];
+  // }
+  Blog[] = [];
 
   constructor(private blogService: BlogService) {
     this.blogs = this.blogService.getBlogs();
   }
   ngOnInit(): void {}
+  executeEdit(blog: Blog) {
+    console.log(blog.id);
+  }
+
+  executeDelete(blog: Blog) {
+    console.log(blog.id);
+  }
 
   executeAction(blog: Blog) {
     console.log(blog.id);
