b6768b26a482d56ebb28365026738fcfd179d1e3
ferminpatdujr <fpatdu@77global.biz>
2022-08-20 20:55:45 +0800

    push changes

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -5,18 +5,30 @@ import { BlogItemComponent } from './components/blog-item/blog-item.component';
 import { BlogRoutingModule } from './blog-routing.module';
 import { CommandBarComponent } from '../../shared/components/command-bar/command-bar.component';
 import { BlogFormComponent } from './pages/blog-form/blog-form.component';
-
-
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    BlogListComponent,
-    BlogItemComponent,
-    BlogFormComponent,
+  declarations: [BlogListComponent, BlogItemComponent, BlogFormComponent],
+  imports: [
+    CommonModule,
+    BlogRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule
 
   ],
-  imports: [
-    CommonModule,BlogRoutingModule,
-  ]
 })
 export class BlogModule { }
