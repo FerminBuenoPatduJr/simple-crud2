8e12729f4458f28124a5d0074dbdb9822b8b9b06
ferminpatdujr <fpatdu@77global.biz>
2022-08-21 07:56:09 +0800

    push changes

--- src/app/modules/book/book.module.ts
+++ src/app/modules/book/book.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookListComponent } from './pages/book-list/book-list.component';
 import { BookItemComponent } from './components/book-item/book-item.component';
 import { BookRoutingModule } from './book-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { BookFormComponent } from './pages/book-form/book-form.component';
 
 @NgModule({
   declarations: [BookListComponent, BookItemComponent],
   imports: [CommonModule, BookRoutingModule, FormsModule, ReactiveFormsModule],
+
+  // exports: [FormBuilder],
+  providers: [FormBuilder],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class BookModule {}
+export class BookModule { }
